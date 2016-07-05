892e9007c0e63c9fc70d32215980fb1d47fdda4d
taryngabbert@gmail.com <taryngabbert@gmail.com>
2016-07-05 15:47:11 -0500

    the value [0], doesnt work

--- server/User/userCtrl.js
+++ server/User/userCtrl.js
@@ -6,9 +6,9 @@ module.exports = {
       const userObject= {
         facebookID: req.user.id
       , facebookName: req.user.displayName
-      , email: req.user.emails[0].value
+      // , email: req.user.emails[0].value
       , username: req.user.username
-      , picture: req.user.photos[0].value
+      // , picture: req.user.photos[0].value
       , firstName: req.user.first_name
       , lastName: req.user.last_name
       , gender: req.user.gender
