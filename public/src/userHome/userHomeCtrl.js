cc8a0e26ec41188565968d3983e5e387183d96cb
taryngabbert@gmail.com <taryngabbert@gmail.com>
2016-07-05 16:47:12 -0500

    restructure object

--- public/src/userHome/userHomeCtrl.js
+++ public/src/userHome/userHomeCtrl.js
@@ -16,7 +16,8 @@ angular.module("providerApp")
 
 
   function checkIfUser() {
-    getFbData().then( (fbData) => {
+    providerService.FBinfo().then( (response) => {
+      var fb = response.data;
       userService.getUsers().then( (users) => {
         for (let i=0; i < users.length; i++) {
           //CHECKS TO SEE IF A USER EXSIST, RETURNS PARAM.ID
@@ -27,15 +28,15 @@ angular.module("providerApp")
           } //IF USER DOESN'T EXSIST, CREATES A NEW USER WITH THE AVAILABLE DATA
           else {
             const newUser =   {
-                  facebookName: fbData.facebookName
-                , facebookID: fbData.id
-                , firstName: fbData.firstName
-                , lastName: fbData.lastName
-                , photos: fbData.picture
-                , email: fbData.email
-                , gender: fbData.gender
-                , birthday: fbData.birthday
-                , location: fbData.location
+                  facebookName: fb.facebookName
+                , facebookID: fb.id
+                , firstName: fb.firstName
+                , lastName: fb.lastName
+                , photos: fb.picture
+                , email: fb.email
+                , gender: fb.gender
+                , birthday: fb.birthday
+                , location: fb.location
               }
               return userService.addUser(newUser);
           }
