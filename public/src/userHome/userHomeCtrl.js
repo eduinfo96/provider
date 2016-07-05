7bce88e1dd4e98a28c491fef2fd31bc28a135b47
taryngabbert@gmail.com <taryngabbert@gmail.com>
2016-07-05 17:10:41 -0500

    cant figure out whats wrong take 3

--- public/src/userHome/userHomeCtrl.js
+++ public/src/userHome/userHomeCtrl.js
@@ -4,37 +4,14 @@ angular.module("providerApp")
 
   $('#profileSetup').modal('show')
 
+
   function checkIfUser() {
     providerService.FBinfo().then( (response) => {
       var fb = response.data;
       console.log(fb);
-      userService.getUsers().then( (users) => {
-        if (!users) {
-          const newUser =   {
-              facebookName: fb.facebookName
-            , facebookID: fb.id
-            , firstName: fb.firstName
-            , lastName: fb.lastName
-            , photos: fb.picture
-            , email: fb.email
-            , gender: fb.gender
-            , birthday: fb.birthday
-            , location: fb.location
-          }
-          console.log("user created!");
-          return userService.addUser(newUser);
-        };
-        for (let i=0; i < users.length; i++) {
-          //CHECKS TO SEE IF A USER EXSIST, RETURNS PARAM.ID
-          if (users[i].facebookID == fb.id) {
-            $scope.user = users[i];
-            console.log("user exsist", users[i]._id);
-            return users[i]._id;
-          } //IF USER DOESN'T EXSIST, CREATES A NEW USER WITH THE AVAILABLE DATA
-          else {
             const newUser =   {
                   facebookName: fb.facebookName
-                , facebookID: fb.id
+                , facebookID: fb.facebookID
                 , firstName: fb.firstName
                 , lastName: fb.lastName
                 , photos: fb.picture
@@ -45,12 +22,39 @@ angular.module("providerApp")
               }
               console.log("user created!");
               return userService.addUser(newUser);
-          }
-        } //end of loop
-      })
-    })
+          })
+        }
+      // userService.getUsers().then( (users) => {
+      //   console.log(users)
+      //   for (let i=0; i < users.length; i++) {
+      //     //CHECKS TO SEE IF A USER EXSIST, RETURNS PARAM.ID
+      //     if (users[i].facebookID == fb.facebookID) {
+      //       $scope.user = users[i];
+      //       console.log("user exsist", users[i]._id);
+      //       return users[i]._id;
+      //     } //IF USER DOESN'T EXSIST, CREATES A NEW USER WITH THE AVAILABLE DATA
+      //     else {
+      //       const newUser =   {
+      //             facebookName: fb.facebookName
+      //           , facebookID: fb.facebookID
+      //           , firstName: fb.firstName
+      //           , lastName: fb.lastName
+      //           , photos: fb.picture
+      //           , email: fb.email
+      //           , gender: fb.gender
+      //           , birthday: fb.birthday
+      //           , location: fb.location
+      //         }
+      //         console.log("user created!");
+      //         return userService.addUser(newUser);
+      //     }
+      //   } //end of loop
+      // })
+  //   })
+  //
+  // }
 
-  }
+
   checkIfUser();
 
   // needs to go in edit user
