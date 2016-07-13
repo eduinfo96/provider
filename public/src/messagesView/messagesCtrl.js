b25aa7795c6766917b2e8b4e527770e03e8d94ba
taryngabbert@gmail.com <taryngabbert@gmail.com>
2016-07-12 20:31:13 -0500

    messages view is now getting sent and recieved messages

--- public/src/messagesView/messagesCtrl.js
+++ public/src/messagesView/messagesCtrl.js
@@ -1,7 +1,29 @@
 angular.module("providerApp")
-.controller("messagesCtrl", function($scope, $rootScope, providerService, userService, appService) {
+.controller("messagesCtrl", function($scope, $rootScope, messageService) {
   $scope.currentUser = $rootScope.currentUser;
-  $scope.userServices = $scope.currentUser.servicesOffered
+  $scope.userServices = $scope.currentUser.servicesOffered;
+
+
+  function getSent() {
+    messageService.getSentMessages($scope.currentUser._id).then( (response) => {
+      $scope.sentMessages = response.data;
+    })
+  }
+  getSent();
+
+  function getUsersInbox() {
+    messageService.getUsersInbox($scope.currentUser._id).then( (response) => {
+      $scope.userInboxMessages = response.data;
+    })
+  }
+  getUsersInbox();
+
+$scope.deleteMessage=(id)=>{
+  messageService.deleteMessage(id).then((response)=>{
+    getUsersInbox();
+    getSent();
+  })
+}
 
 
 
